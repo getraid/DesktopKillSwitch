646f00a637e43558f6c12905aea898f466ba3dcc
GetRaid <32432718+getraid@users.noreply.github.com>
2019-09-30 03:13:43 +0200

    Started off with some first code

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-const axios = require('axios')
-var ping = require('ping');
+const axios = require("axios");
+var ping = require("ping");
+
+const express = require("express");
+const server = express();
+
+server.post("/shutdown", (req, res) => {
+  res.sendStatus(200);
+  //do stuff
+});
+
+server.get("/desktopkillswitch", (req, res) => {
+  res.sendStatus(200);
+  //do stuff
+});
+
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const port = 5123;
+
+server.listen(port, () => {
+  console.log(`Server listening at ${port}`);
+});
